c8b8cac2ee914d55cf2ad54640a72c5fb7b64c3e
LiliValdovinos <lilianavaldovinos36@gmail.com>
2025-03-05 09:50:54 -0600

    feat(Interface login): add login window

--- Psytech/app.js
+++ Psytech/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const path = require('path');
+// Servir archivos estáticos desde la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuración de EJS como motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Middleware para procesar datos del formulario
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Importar rutas
 const rutasInicioSesion = require('./routes/inicioSesion.routes');
 app.use('/', rutasInicioSesion);
 
@@ -15,12 +27,7 @@ app.use('/aspirante', rutasAspirante);
 const rutasPsicologo = require('./routes/psicologo.routes');
 app.use('/psicologo', rutasPsicologo);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.listen(5050);
+// Iniciar servidor en el puerto 5050
+app.listen(5050, () => {
+    console.log("Servidor corriendo en http://localhost:5050");
+});
