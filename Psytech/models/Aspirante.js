b9efc510b37712ba914b4600f873be9f0a6160f9
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2025-03-21 16:21:24 -0600

    feat(models): Add Pais, Estado and Aspirante models; Pair Programming: Fer

---
+++ Psytech/models/Aspirante.js
@@ -0,0 +1,45 @@
+const db = require('../util/database');
+
+module.exports = class Aspirante {
+
+    constructor(infoAspirante) {
+        this.nombreUsuario = infoAspirante.nombreUsuario,
+        this.apellidoPaterno = infoAspirante.apellidoPaterno,
+        this.apellidoMaterno = infoAspirante.apellidoMaterno,
+        this.correo = infoAspirante.correo,
+        this.lada = infoAspirante.lada,
+        this.numeroTelefono = infoAspirante.numeroTelefono,
+        this.usuario = this.correo,
+        this.contrasenia = this.nombreUsuario,
+        this.institucionProcedencia = infoAspirante.institucionProcedencia,
+        this.idPais = infoAspirante.idPais,
+        this.idEstado = infoAspirante.idEstado
+    }
+
+    saveUsuario(){
+        db.execute(`
+            INSERT INTO Usuarios VALUES 
+            (uuid(), ?, ?, true, ?, ?, ?, ?, ?, ?, 
+            (SELECT idRol FROM Roles WHERE nombreRol = 'Aspirante')
+            )`, 
+            [this.usuario, this.contrasenia, this.nombreUsuario, 
+                this.apellidoPaterno, this.apellidoMaterno, this.correo, 
+                this.lada, this.numeroTelefono]);
+    }
+    getIdUsuario(){
+        return db.execute(`
+            SELECT idUsuario
+            FROM Usuarios
+            WHERE nombreUsuario = ?
+            `, 
+            [this.nombreUsuario]);
+    }
+
+    saveAspirante(idUsuario){
+        db.execute(`
+            INSERT INTO Aspirantes VALUES (uuid(), ?, ?, ?, ?, null, null)
+            `, 
+            [idUsuario, this.institucionProcedencia, this.idPais, this.idEstado]);
+    }
+
+}
