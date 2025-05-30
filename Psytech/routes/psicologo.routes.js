4467cba41c6c6e8c1952d8ab0b2aa4bea5e6a937
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-05-29 22:49:14 -0600

    feat(booklet Terman): routes for the booklet of terman

--- Psytech/routes/psicologo.routes.js
+++ Psytech/routes/psicologo.routes.js
@@ -110,6 +110,7 @@ router.get("/interpretacion/:idAspirante/:columna/:nivel", controller.get_interp
 router.get('/analisis-terman/:idAspirante/:idGrupo/serie/:idSerie', controller.getRespuestasSerie);
 router.get('/analisis-terman/:idGrupo/:idAspirante/:idInstitucion', controller.getAnalisisTerman);
 router.get('/analisis-terman/:idAspirante/:idGrupo/Test%20de%20Aptitud%20Mental%20TERMAN', controller.getAnalisisTerman);
+router.get('/cuadernillo-Terman/:idGrupo/:idAspirante/:idInstitucion', controller.getCuadernilloTerman);
 
 router.get('/psicologos-registrados', controller.getPsicologosRegistrados);
 
